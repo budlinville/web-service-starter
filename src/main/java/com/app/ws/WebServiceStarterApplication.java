7d6861da2d85739d3810d85398dc42b3d87a2d9e
Bud Linville <a0l035t@homeoffice.wal-mart.com>
2019-11-19 11:40:16 -0600

    first commit

---
+++ src/main/java/com/app/ws/WebServiceStarterApplication.java
@@ -0,0 +1,13 @@
+package com.app.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServiceStarterApplication.class, args);
+	}
+
+}
